a2403d5f2ff36a266fd7556549cc1ac2964305f1
itsparwezali <itsparwezali@gmail.com>
2021-09-15 20:52:50 +0545

    fist c

---
+++ src/main/java/com/example/Mango/MangoApplication.java
@@ -0,0 +1,14 @@
+package com.example.Mango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoApplication.class, args);
+		System.out.println("mango,...");
+	}
+
+}
---
+++ src/test/java/com/example/Mango/MangoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
